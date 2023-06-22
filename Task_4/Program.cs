fd8c097abfd7ec56f61baee3307632ce7aea6baf
Ruslan <rus05vorobjev@example.com>
2023-06-22 21:02:22 +0300

    Задание 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+ arr[i, j] = random.Next(1, 10);
+ Console.Write(arr[i, j] + " ");
+    }
+ Console.WriteLine();
+}
+double sum = 0;
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        if(i == j)
+        {
+          sum += arr[i, j];
+        }
+        
+    }
+ 
+}
+Console.Write($"Cумма элементов, находящихся на главной диагонали: {sum}");
